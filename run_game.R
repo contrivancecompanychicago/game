a95f34b0793a3dcb820590cd2e4e20c7028323cb
aggelosk <koropoulis.a@gmail.com>
2019-04-09 13:16:59 +0300

    Add files via upload

--- run_game.R
+++ run_game.R
@@ -12,14 +12,13 @@ name <- ""
 seed <- sample(1:100000000, n,  replace=FALSE)
 write.table(seed, file="seeds.txt", quote=F, row.names=F, col.names=F)
 
-cmds <- paste(curDir, "/game -pred 10000 -prey 4 -pinf 3 0 2 3 -rnds 100000 -burn 10000 -smpl 10000 50 -smpl 18000 50 -bttl 10000 1000 -bttl 12000 10000 -seed ", seed, sep="")
+cmds <- paste(curDir, "/game -pred 1000 -prey 4 -pinf 3 0 2 3 -rnds 10000 -burn 1000 -smpl 10000 10 -synr 0.33 -comr 0.66 -seed ", seed, sep="")
 write.table(x=cmds, file="cmd.txt", append=F, quote=F, row.names=F, col.names=F)
 
 
 foreach(i =  1:n)%dopar%{
-    #system(paste("rm -r run.", i, sep=""))
     setwd(file.path(curDir, name))
-    dirName <- paste("run", i, sep="")
+    dirName <- paste("rungen", i, sep="")
     working_dir<-file.path(paste(curDir, "/", name, sep=""), dirName)
     dir.create(working_dir, showWarnings=FALSE)
     setwd(working_dir)
